248285def19993bd339b9e9fe8487486f8670b5e
t77bsh <100529283+t77bsh@users.noreply.github.com>
2022-10-04 14:50:50 +0100

    refactored

--- PermutingTwoArrays.py
+++ PermutingTwoArrays.py
@@ -5,6 +5,4 @@
 def twoArrays(k, A, B):
     C = [k-b for b in B]
     ans = [1 for c in C if sum(a >= c for a in A) >= C.count(c)]
-    if sum(ans) == len(B):
-        return 'YES'
-    return 'NO'
+    return 'YES' if sum(ans) == len(B) else 'NO'
